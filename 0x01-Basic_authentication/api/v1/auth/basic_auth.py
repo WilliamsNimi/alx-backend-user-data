b47c99d1261301090369e4115384b25a56ab6598
Nimi Williams <williamson.nimi@gmail.com>
2024-04-18 21:57:47 +0100

    syntax errors fixed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,9 +61,9 @@ class BasicAuth(Auth):
         @user_pwd: the password of the user
         Return: returns the user instance
         """
-        if user_email is None or type(user_email) is not a str:
+        if user_email is None or type(user_email) is not str:
             return None
-        if user_pwd is None or type(user_pwd) is not a str:
+        if user_pwd is None or type(user_pwd) is not str:
             return None
         try:
             users = User.search({'email': user_email})
