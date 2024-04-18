1cb646a9ce279b7d21bd42631e589432a6f735ca
Nimi Williams <williamson.nimi@gmail.com>
2024-04-18 19:36:03 +0100

    updated the require auth function for route validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Authentication module """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -11,8 +12,17 @@ class Auth:
         """ ensures authentication is required
         @path: Argument for the route path
         @excluded_paths: Argument for paths that don't need authentication
-        Return: Returns False """
-        return False
+        Return: Returns True or False based on exitence of Path """
+        if path is None or excluded_paths is None:
+            return True
+        if excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for route in excluded_paths:
+            if re.match(route.replace('*', '.*'), path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ adding authentication to the request header
