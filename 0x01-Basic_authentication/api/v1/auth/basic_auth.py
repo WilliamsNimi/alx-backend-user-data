976045f581fcfa6592f7350bc5e791bc53949675
Nimi Williams <williamson.nimi@gmail.com>
2024-04-18 21:14:51 +0100

    pycodestyle fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,17 +3,19 @@
 from api.v1.auth.auth import Auth
 import base64
 
+
 class BasicAuth(Auth):
     """ The basic Auth class extends Auth"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ gettin base64 header
         @authorization_header: the authorization header to be authenticated
         Return: Returns base64 of the header
         """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or
+        type(authorization_header) is not str:
             return None
         if authorization_header.startswith('Basic '):
             return authorization_header[6:]
         return None
-        
