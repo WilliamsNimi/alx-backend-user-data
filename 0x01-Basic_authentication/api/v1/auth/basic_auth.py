5fab673f4f1f9bc57b41af48f7c0a52503c9367a
Nimi Williams <williamson.nimi@gmail.com>
2024-04-18 20:27:03 +0100

    Base64 encoding for basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 """ the Basic Auth class """
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ The basic Auth class extends Auth"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ gettin base64 header
+        @authorization_header: the authorization header to be authenticated
+        Return: Returns base64 of the header
+        """
+        if authorization_header is None or type(authorization_header) is not str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header[6:]
+        return None
+        
