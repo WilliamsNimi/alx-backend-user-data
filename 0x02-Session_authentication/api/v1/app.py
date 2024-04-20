93416afbbda38583fb675882fcd2310113637eed
Nimi Williams <williamson.nimi@gmail.com>
2024-04-20 22:41:49 +0100

    updating route list for authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,10 +33,12 @@ def before_request():
         return
     route_list = ['/api/v1/status/',
                   '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+                  '/api/v1/forbidden/',
+                  '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, route_list):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and
+    auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
