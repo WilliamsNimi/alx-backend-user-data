e8bf1a4fcb26bda305eaff59e0116df9b12135d4
Nimi Williams <williamson.nimi@gmail.com>
2024-04-20 21:44:57 +0100

    creating session id using uuid4

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 """ session authentication module"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Sesstion Authentication class """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ session creation method
+        @user_id: user id with which to create a session
+        Return: Returns a sessionID
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        user_id_by_session_id[session_id] = user_id
+        return session_id
