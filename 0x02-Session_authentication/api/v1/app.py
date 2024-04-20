266550bb51b299a286d5baac8bbe7f8404ca959f
Nimi Williams <williamson.nimi@gmail.com>
2024-04-20 21:23:12 +0100

    Added an empty session authentication class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,7 +13,10 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-if getenv('AUTH_TYPE') == 'basic_auth':
+if gentenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
