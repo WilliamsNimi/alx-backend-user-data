4de22ded59ee9cf21daf9aebf4a18056319a37d1
Nimi Williams <williamson.nimi@gmail.com>
2024-04-21 19:53:37 +0100

    Added logout authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,14 @@ def login_auth() -> str:
             response.set_cookie(session_name, session_id)
             return response
     return jsonify({'error': 'wrong password'}), 401
+
+@app_views.route('auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def logout_auth() -> str:
+    """ Log out user from session"""
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    return False, abort(404)
