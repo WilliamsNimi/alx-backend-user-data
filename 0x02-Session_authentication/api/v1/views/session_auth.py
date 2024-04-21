1779db61666add106c7a4788769fbc997c261c16
Nimi Williams <williamson.nimi@gmail.com>
2024-04-21 19:42:57 +0100

    login authentication creation

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Session authentication module for users"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request, make_response
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login',
+                 methods=['POST'],
+                 strict_slashes=False)
+def login_auth() -> str:
+    """ Login authentication"""
+    email = request.form.get('email')
+    if email is None or email == '':
+        return jsonify({'error': 'email missing'}), 400
+    password = request.form.get('password')
+    if password is None or password == '':
+        return jsonify({'error': 'password missing'}), 400
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify({'error': 'no user found for this email'}), 400
+
+    if users is None:
+        return jsonify({'error': 'no user found for this email'}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            session_name = getenv('SESSION_NAME')
+            response = jsonify(user.to_json())
+            response.set_cookie(session_name, session_id)
+            return response
+    return jsonify({'error': 'wrong password'}), 401
