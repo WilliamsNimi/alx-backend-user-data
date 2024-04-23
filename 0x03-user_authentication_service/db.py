0097074089eb35450bf63a928722882c10fb88fe
Nimi Williams <williamson.nimi@gmail.com>
2024-04-23 21:23:30 +0100

    Added try and exception

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,11 @@ class DB:
         @hashed_password: the hashed password fo the user
         Return: Returns the new user object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
