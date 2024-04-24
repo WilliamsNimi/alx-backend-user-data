e00ebd768471f8091cc827873e53f6957d12a764
Nimi Williams <williamson.nimi@gmail.com>
2024-04-24 18:57:40 +0100

    login authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,3 +39,16 @@ class Auth:
             return user
         except Exception:
             raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Login validation method
+        @email: email of the user
+        @password: user password
+        Return: Returns True or False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            p_bytes = password.encode('utf-8')
+            return bycrypt.checkpw(p_bytes, user.hashed_password)
+        except:
+            return False
