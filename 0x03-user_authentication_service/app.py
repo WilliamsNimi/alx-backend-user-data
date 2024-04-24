0f116d9803765d7e2c83ad464522b8f6985300b3
Nimi Williams <williamson.nimi@gmail.com>
2024-04-24 18:41:53 +0100

    Created a create user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,13 +2,30 @@
 """ Basic Flask application"""
 from flask import Flask, jsonify
 app = Flask(__name__)
+from auth import Auth
+AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes= False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def getRequest() -> str:
     """basic get request"""
     return jsonify({"message": "Bienvenue"})
 
-
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """User authentication function
+    @email: email of the user
+    @password: user password
+    Return: Returns nothing
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
