60aecfc935ebaf6b101cafb7beb6deaa9ea664fc
Nimi Williams <williamson.nimi@gmail.com>
2024-04-24 11:31:57 +0100

    created a user registration method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,3 +12,30 @@ def _hash_password(password: str):
     p_salt = bcrypt.gensalt()
     p_hash = bcrypt.hashpw(p_bytes, p_salt)
     return p_hash
+
+
+from db import DB
+from user import User
+from typing import TypeVar
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """ Registers the User
+        @email: Email of the new user
+        @password: password of the new user
+        Return: Returns the new User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            user = User(email=email, password=_hashed_password(password))
+            self._db.add_user(user)
+            return user
+        except Exception:
+            raise ValueError("User {} already exists".format(email))
