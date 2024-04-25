2aec8a6aa49d0b4e99734de273da497ce90383d6
Nimi Williams <williamson.nimi@gmail.com>
2024-04-25 14:30:48 +0100

    password reset route created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,18 @@ def profile():
     return 200, jsonify({"email": "{}".format(user.email)})
 
 
+@app.route('/reset_password', method=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ Password generation route"""
+    email = request.form.get('email')
+    if not email:
+        abort(403)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return 200, jsonify({"email": email, "reset_token": reset_tokem})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
