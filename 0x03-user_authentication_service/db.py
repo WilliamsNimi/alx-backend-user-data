3a951c598dba4f84fa1832ddfc1e01c8dbb499ab
Nimi Williams <williamson.nimi@gmail.com>
2024-04-25 14:52:45 +0100

    type fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,7 +48,7 @@ class DB:
             new_user = None
         return new_user
 
-    def find_user_by(self, **kwargs: Mapping) -> User:
+    def find_user_by(self, **kwargs: Mapping):
         """ A method to search the db
         @query_str: the query to search for
         Return: Returns the first row where the query is found
