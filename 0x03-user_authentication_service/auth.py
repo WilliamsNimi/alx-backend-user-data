75524de613ada813926e844d64530d1effe35d29
Nimi Williams <williamson.nimi@gmail.com>
2024-04-27 21:06:46 +0100

    Function logic change auth.py register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,10 +39,10 @@ class Auth:
         try:
             self._db.find_user_by(email=email)
         except Exception:
-            raise ValueError("User {} already exists".format(email))
-        user = User(email=email, password=_hashed_password(password))
-        self._db.add_user(user)
-        return user
+            user = User(email=email, password=_hashed_password(password))
+            self._db.add_user(user)
+            return user
+        raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Login validation method
