6bd3d75f7f89e677dc17ca2f0a18eb262e267b88
Nimi Williams <williamson.nimi@gmail.com>
2024-04-27 21:42:14 +0100

    Added a try except block to destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,8 +89,11 @@ class Auth:
         @user_id: User id to find user whose session is to be destroyed
         Return: None
         """
-        self._db.update_user(user_id, session_id=None)
-        return None
+        try:
+            user = self._db.find_user_by(id=user_id)
+            return self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """ password reset function"""
